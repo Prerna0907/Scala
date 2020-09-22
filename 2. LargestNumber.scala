18fa53766229efd806b8d5c35c8e8226c6f2adaa
Prerna Pandey <46298446+Prerna0907@users.noreply.github.com>
2020-09-22 09:35:10 +0530

    Create 2. LargestNumber.scala

---
+++ 2. LargestNumber.scala
@@ -0,0 +1,17 @@
+//Program to print largest of three numbers
+
+var x = 13
+
+var y = 87
+
+var z = 45
+
+if ((x>y)&&(x>z))
+print (x)
+
+if ((y>x)&&(y>z))
+print (y)
+//Output 87
+
+if ((z>x)&&(z>y))
+print (z)
