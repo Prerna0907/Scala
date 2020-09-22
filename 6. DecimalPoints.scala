22dc9af6aafee4c284ef49f162cb9e0fa91838f4
Prerna Pandey <46298446+Prerna0907@users.noreply.github.com>
2020-09-22 10:09:28 +0530

    Create 6. DecimalPoints.scala

---
+++ 6. DecimalPoints.scala
@@ -0,0 +1,9 @@
+//Program to print the cost of 10 glazed donuts when it is given that price of one donut is 2.50, uoto two decimal points
+
+var price = 2.50
+
+price * 10
+//Output res1 : Double = 25.0
+
+print("Total cost of 10 glazed donuts is ") ; f"$res1%1.2f"
+//Output Total cost of 10 glazed donuts is : res6 : String = 25.00
