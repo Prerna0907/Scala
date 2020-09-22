7ab4702033dcfdcb63b878a041797e02fbd70dca
Prerna Pandey <46298446+Prerna0907@users.noreply.github.com>
2020-09-22 09:32:33 +0530

    Create 1. variables.scala

---
+++ 1. variables.scala
@@ -0,0 +1,19 @@
+//Declaring Variables
+
+var x = 10;
+//Output x : Int = 10
+
+var y =2.5
+//Output y : Double = 2.5
+
+print(x+y)
+//Output 12.5
+
+print(x*y)
+//Output 25.0
+
+print(x-y)
+//Output 7.5
+
+print(x/y)
+//Output 4.0
