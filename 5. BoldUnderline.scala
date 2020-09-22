8e42663538fa6e150a1229afe11b85c4de77fbb0
Prerna Pandey <46298446+Prerna0907@users.noreply.github.com>
2020-09-22 09:55:17 +0530

    Create 5. BoldUnderline.scala

---
+++ 5. BoldUnderline.scala
@@ -0,0 +1,11 @@
+// Program to print name in bold and age in underlined text
+
+var name = readLine ( )
+
+var age = readInt ( )
+
+print (Console.BOLD)
+print (name)
+
+print(Console.UNDERLINED)
+print(age)
