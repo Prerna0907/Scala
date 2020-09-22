694467b8f836e39227e48602633b6e78e5ae5377
Prerna Pandey <46298446+Prerna0907@users.noreply.github.com>
2020-09-22 09:43:27 +0530

    Create 4. PositiveorNegative.scala

---
+++ 4. PositiveorNegative.scala
@@ -0,0 +1,14 @@
+//Program to print whether a number is positive or negative
+
+var x = readInt( )
+//Output x : Int = -50
+
+if (x>0)
+print ("Positive Number")
+
+if (x<0)
+print ("Negative number")
+//Output Negative number
+
+if (x==0)
+print ("Zero")
